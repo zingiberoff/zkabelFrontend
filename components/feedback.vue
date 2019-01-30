4d70199a308bf200b53faeefdac8f5fce233daea
zeta_kabel <yaroslav.zeta@gmail.com>
2019-01-30 13:53:23 +0700

    Create fast order page

---
+++ components/feedback.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-content>
+    <v-btn>Заказать</v-btn>
+  </v-content>
+</template>
+
+<script>
+    export default {
+        name: "feedback"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/fast-order.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script>
+  import Category from '../../components/Category.vue'
+    export default {
+        name: "fast-order"
+    }
+</script>
+
+<style scoped>
+
+</style>
