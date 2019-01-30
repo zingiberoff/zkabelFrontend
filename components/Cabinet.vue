5878671f33c37bc96127efea116c50e6019e88ac
zeta_kabel <yaroslav.zeta@gmail.com>
2019-01-30 13:53:46 +0700

    Create pages

---
+++ components/Cabinet.vue
@@ -0,0 +1,16 @@
+<template>
+    <nuxt-error :error="{message:'Данный раздел находится в разработке',statusCode:404}"/>
+</template>
+
+<script>
+
+    import NuxtError from "../.nuxt/components/nuxt-error";
+    export default {
+        name: "Cabinet",
+      components: {NuxtError}
+    }
+</script>
+
+<style scoped>
+
+</style>
