f4d3e5b458efc141a9f25777ab521ac0894e2a4a
zeta_kabel <yaroslav.zeta@gmail.com>
2019-01-30 13:56:26 +0700

    Create pages

---
+++ pages/profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <cabinet v-if="$store.state.userToken"/>
+  <auth v-else/>
+
+</template>
+
+<script>
+  import Auth from "../components/Auth";
+  import Cabinet from "../components/Cabinet";
+
+  export default {
+    name: "profile",
+    components: {Auth,Cabinet}
+  }
+</script>
+
+<style scoped>
+
+</style>
